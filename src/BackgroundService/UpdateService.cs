fb7c7635f5b28a8a6011b90e55b5538f241eabb0
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-08-15 13:57:01 -0700

    initial commit

---
+++ src/BackgroundService/UpdateService.cs
@@ -0,0 +1,42 @@
+﻿namespace DynIpUpdater
+{
+    public class UpdateService(ILogger<UpdateService> logger, IAddrFetcher addrFetcher)
+        : BackgroundService
+    {
+        private readonly ILogger<UpdateService> _logger = logger;
+        private readonly IAddrFetcher _addrFetcher = addrFetcher;
+
+        public IAddress? CurrentAddress { get; set; }
+
+        public bool AddressSet
+        {
+            get => CurrentAddress is not null;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (!AddressSet)
+                {
+                    CurrentAddress = await _addrFetcher.FetchAddressAsync();
+                    if (_logger.IsEnabled(LogLevel.Information))
+                    {
+                        _logger.LogInformation("Ip Address is {address}", CurrentAddress.Address);
+                    }
+                }
+                else
+                {
+                    if (_logger.IsEnabled(LogLevel.Information))
+                    {
+                        _logger.LogInformation(
+                            "Ip Address is already set to {address}",
+                            CurrentAddress?.Address
+                        );
+                    }
+                }
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
