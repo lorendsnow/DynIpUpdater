e6af3156408afad4aec18619f2402e2208b2fff4
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-08-15 16:55:38 -0700

    update usings

--- src/GlobalUsings.cs
+++ src/GlobalUsings.cs
@@ -1,4 +1,5 @@
-﻿global using System.Net.Http.Json;
+﻿global using System.ComponentModel.DataAnnotations;
+global using System.Net.Http.Json;
 global using System.Text.Json.Serialization;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
