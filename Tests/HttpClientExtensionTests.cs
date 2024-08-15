02edbae12bf3c827a7249df554e436ffea921856
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-08-15 15:16:40 -0700

    initial commit

---
+++ Tests/HttpClientExtensionTests.cs
@@ -0,0 +1,27 @@
+﻿using DynIpUpdater;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Xunit.Abstractions;
+
+namespace Tests
+{
+    public class HttpClientExtensionTests(ITestOutputHelper helper)
+    {
+        private readonly ITestOutputHelper _helper = helper;
+
+        [Fact]
+        public void AddIpifyClient_ClientAdded()
+        {
+            var builder = Host.CreateApplicationBuilder();
+
+            builder.Services.AddIpifyClient();
+
+            var app = builder.Build();
+            using HttpClient client = app
+                .Services.GetRequiredService<IHttpClientFactory>()
+                .CreateClient(NamedHttpClients.IpifyFetcher.ToString());
+
+            Assert.True(client.BaseAddress == new Uri("https://api.ipify.org"));
+        }
+    }
+}
