dc73236be2569b799780bdd8edbb64de11b4c2c3
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-08-15 13:21:45 -0700

    initial commit

---
+++ Tests/IpifyClientFactoryTests.cs
@@ -0,0 +1,17 @@
+﻿using DynIpUpdater;
+
+namespace Tests
+{
+    public class IpifyClientFactoryTests
+    {
+        [Fact]
+        public void CreateIpifyClient_ReturnsHttpClient()
+        {
+            var expected = new HttpClient() { BaseAddress = new Uri("https://api.ipify.org") };
+
+            var actual = IpifyClientFactory.CreateClient();
+
+            Assert.Equal(expected.BaseAddress, actual.BaseAddress);
+        }
+    }
+}
---
+++ src/HttpClient/IpifyClientFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace DynIpUpdater
+{
+    /// <summary>
+    /// Simple static class which handles creating single HttpClient instances for fetching IP
+    /// addresses.
+    /// </summary>
+    public static class IpifyClientFactory
+    {
+        /// <summary>
+        /// Creates a new HttpClient instance with the base address set to ipify.org.
+        /// </summary>
+        /// <returns>
+        /// A new <see cref="HttpClient"/> instance with the base address set to ipify.org.
+        /// </returns>
+        public static HttpClient CreateClient()
+        {
+            return new HttpClient() { BaseAddress = new Uri("https://api.ipify.org") };
+        }
+    }
+}
