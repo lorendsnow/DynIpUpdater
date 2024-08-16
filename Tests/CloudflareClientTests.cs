c923b07b69edd77abf1d8b5c10f448e99adb693b
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-08-15 23:01:03 -0700

    initial commit

---
+++ Tests/CloudflareClientTests.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DynIpUpdater;
+
+namespace Tests
+{
+    public class CloudflareClientTests
+    {
+        [Fact]
+        public void UpdateDnsRecordRequest_NameTooLong_ThrowsValidationException()
+        {
+            Assert.Throws<ValidationException>(() =>
+            {
+                UpdateDnsRecordRequest request =
+                    new(
+                        "123.123.123.123",
+                        new string('a', 658),
+                        "A",
+                        "023e105f4ecef8ad9ca31a8372d0c353"
+                    );
+            });
+        }
+
+        [Fact]
+        public void UpdateDnsRecordRequest_IdTooLong_ThrowsValidationException()
+        {
+            Assert.Throws<ValidationException>(() =>
+            {
+                UpdateDnsRecordRequest request =
+                    new("123.123.123.123", "example.com", "A", new string('a', 33));
+            });
+        }
+
+        [Fact]
+        public void UpdateDnsRecordRequest_RecordTypeInvalid_ThrowsValidationException()
+        {
+            Assert.Throws<ValidationException>(() =>
+            {
+                UpdateDnsRecordRequest request =
+                    new("123.123.123.123", "example.com", "MX", "12345");
+            });
+        }
+    }
+}
