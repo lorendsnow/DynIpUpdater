c229f777dafd699d64f7452278861559003c5b2c
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-08-18 14:17:06 -0700

    Add docstring comments

--- src/Models/CloudflareConfiguration.cs
+++ src/Models/CloudflareConfiguration.cs
@@ -1,9 +1,20 @@
 ﻿namespace DynIpUpdater
 {
+    /// <summary>
+    /// Represents a configuration for Cloudflare records. Supports multiple zones, each with
+    /// multiple records.
+    /// </summary>
     public class CloudflareConfiguration
     {
+        /// <summary>
+        /// A list of <see cref="ZoneConfiguration"/> objects.
+        /// </summary>
         public required List<ZoneConfiguration> Zones { get; set; }
 
+        /// <summary>
+        /// Returns a string representation of the configuration.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return "CloudflareConfiguration "
