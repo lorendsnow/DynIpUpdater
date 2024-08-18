2a42995c25f583125874418afcbb7e2b808fec76
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-08-18 14:18:13 -0700

    add docstring comments

--- src/ConfigurationParsing.cs
+++ src/ConfigurationParsing.cs
@@ -2,6 +2,13 @@
 {
     public static class ConfigurationParsing
     {
+        /// <summary>
+        /// Loads a YAML configuration file and adds a singleton of the configuration to the
+        /// service collection.
+        /// </summary>
+        /// <param name="builder">The program's hostbuilder.</param>
+        /// <param name="path">The relative path to the config file.</param>
+        /// <returns>The program's hostbuilder.</returns>
         public static HostApplicationBuilder ParseConfigFile(
             this HostApplicationBuilder builder,
             string path
