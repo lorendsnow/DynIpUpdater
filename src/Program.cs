bf2c3cd6f1fb1268b1ac17bc2384d65862b0bcd3
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-08-18 14:18:53 -0700

    add config parsing functionality and cloudflare client

--- src/Program.cs
+++ src/Program.cs
@@ -4,10 +4,20 @@
     {
         public static void Main(string[] args)
         {
+            string configPath = args.Length > 0 ? args[0] : string.Empty;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please provide a path to a configuration file.");
+                return;
+            }
+
             HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+            builder.ParseConfigFile(configPath);
 
             builder.Services.AddHostedService<UpdateService>();
             builder.Services.AddIpifyClient();
+            builder.Services.AddCloudflareClient();
 
             IHost host = builder.Build();
 
