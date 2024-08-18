cf2a4f3653f3988b307d76e237a23e40dd9020a8
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-08-18 14:17:53 -0700

    add docstring comments

--- src/Models/ZoneConfiguration.cs
+++ src/Models/ZoneConfiguration.cs
@@ -1,13 +1,39 @@
 ﻿namespace DynIpUpdater
 {
+    /// <summary>
+    /// Represents configuration for a single zone in Cloudflare.
+    /// </summary>
     public class ZoneConfiguration
     {
+        /// <summary>
+        /// Bearer token associated with this zone.
+        /// </summary>
         public required string BearerToken { get; init; }
+
+        /// <summary>
+        /// API key associated with this zone.
+        /// </summary>
         public required string ApiKey { get; init; }
+
+        /// <summary>
+        /// Email associated with this zone.
+        /// </summary>
         public required string Email { get; init; }
+
+        /// <summary>
+        /// Zone ID associated with this zone.
+        /// </summary>
         public required string ZoneId { get; init; }
+
+        /// <summary>
+        /// List of DNS records to be created and/or updated under this zone.
+        /// </summary>
         public required List<DnsRecord> DnsRecords { get; set; }
 
+        /// <summary>
+        /// Returns a string representation of this ZoneConfiguration.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return "ZoneConfiguration "
