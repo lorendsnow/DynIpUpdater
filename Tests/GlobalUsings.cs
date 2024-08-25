b9893999a34ba4968767559222942f535fb7e2bd
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-08-25 13:06:18 -0700

    additional dependencies

--- Tests/GlobalUsings.cs
+++ Tests/GlobalUsings.cs
@@ -3,3 +3,6 @@ global using System.Text.RegularExpressions;
 global using DynIpUpdater;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging.Testing;
+global using Tests.Helpers;
+global using YamlDotNet.Serialization;
