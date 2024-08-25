4891ffed7d6b3568040273cc0b0b127b3841acc8
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-08-25 14:22:50 -0700

    initial commit

---
+++ src/HttpClients/CloudflareClient/Responses/CreateDnsRecordResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace DynIpUpdater
+{
+    public record CreateDnsRecordResponse
+    {
+        public RecordResponse? Result { get; init; }
+        public required ResponseError[] Errors { get; init; }
+        public required ResponseError[] Messages { get; init; }
+        public bool Success { get; init; }
+    }
+}
