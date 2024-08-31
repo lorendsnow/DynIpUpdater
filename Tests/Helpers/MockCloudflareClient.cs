37ba233e1b0f67ef3687e41c90b287f52df280f6
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-08-31 12:48:37 -0700

    update based on changes in program

--- Tests/Helpers/MockCloudflareClient.cs
+++ Tests/Helpers/MockCloudflareClient.cs
@@ -17,7 +17,7 @@
             return await Task.FromResult(_aRecordResponse);
         }
 
-        public async Task<CreateDnsRecordResponse> CreateRecordAsync(
+        public async Task<SingleRecordResponse> CreateRecordAsync(
             CreateDnsRecordRequest request,
             CancellationToken cancellationToken
         )
@@ -25,6 +25,8 @@
             RecordResponse result =
                 new()
                 {
+                    ZoneId = "023e105f4ecef8ad9ca31a8372d0c353",
+                    ZoneName = "example.com",
                     Content = "123.123.123.123",
                     Name = request.Name,
                     Proxied = request.Proxied,
@@ -38,7 +40,39 @@
                 };
 
             return await Task.FromResult(
-                new CreateDnsRecordResponse()
+                new SingleRecordResponse()
+                {
+                    Result = result,
+                    Errors = [],
+                    Messages = [],
+                    Success = true,
+                }
+            );
+        }
+
+        public async Task<SingleRecordResponse> UpdateRecordAsync(
+            UpdateDnsRecordRequest request,
+            CancellationToken cancellationToken
+        )
+        {
+            RecordResponse result =
+                new()
+                {
+                    ZoneId = "023e105f4ecef8ad9ca31a8372d0c353",
+                    ZoneName = "example.com",
+                    Content = "123.123.123.123",
+                    Name = request.Name,
+                    Proxied = request.Proxied,
+                    Type = "A",
+                    CreatedOn = DateTimeOffset.Now,
+                    Id = "023e105f4ecef8ad9ca31a8372d0c353",
+                    Meta = new RecordMeta() { AutoAdded = true, Source = "Source" },
+                    ModifiedOn = DateTimeOffset.Now,
+                    Proxiable = true,
+                    TTL = 1,
+                };
+            return await Task.FromResult(
+                new SingleRecordResponse()
                 {
                     Result = result,
                     Errors = [],
@@ -67,6 +101,8 @@
                 [
                     new RecordResponse()
                     {
+                        ZoneId = "023e105f4ecef8ad9ca31a8372d0c353",
+                        ZoneName = "example.com",
                         Content = "123.123.123.123",
                         Name = "example.com",
                         Proxied = true,
@@ -80,6 +116,8 @@
                     },
                     new RecordResponse()
                     {
+                        ZoneId = "023e105f4ecef8ad9ca31a8372d0c353",
+                        ZoneName = "example.com",
                         Content = "123.123.123.123",
                         Name = "www",
                         Proxied = true,
