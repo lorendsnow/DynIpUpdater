a702f135821aa6f22a8e635c5d79dc6ad1b74248
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-08-31 12:56:39 -0700

    change messages to use the separate responsemessage struct

--- src/HttpClients/CloudflareClient/Responses/SingleRecordResponse.cs
+++ src/HttpClients/CloudflareClient/Responses/SingleRecordResponse.cs
@@ -28,6 +28,6 @@
         /// A list of messages returned by the request.
         /// </summary>
         [JsonPropertyName("messages")]
-        public required ResponseError[] Messages { get; init; }
+        public required ResponseMessage[] Messages { get; init; }
     }
 }
