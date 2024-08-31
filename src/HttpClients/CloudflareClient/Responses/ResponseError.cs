8969319fca24e9b792daec2c062329fa3f63a74e
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-08-31 12:45:57 -0700

    initial commit

---
+++ src/HttpClients/CloudflareClient/Responses/ResponseError.cs
@@ -0,0 +1,19 @@
+﻿namespace DynIpUpdater
+{
+    /// <summary>
+    /// Represents an error returned in a response from the Cloudflare API.
+    /// </summary>
+    public readonly struct ResponseError
+    {
+        [JsonPropertyName("code")]
+        public int Code { get; init; }
+
+        [JsonPropertyName("message")]
+        public string Message { get; init; }
+
+        public override string ToString()
+        {
+            return $"ResponseError: " + $"Code: {Code}, " + $"Message: {Message}";
+        }
+    }
+}
---
+++ src/HttpClients/CloudflareClient/Responses/ResponseMessage.cs
@@ -0,0 +1,19 @@
+﻿namespace DynIpUpdater
+{
+    /// <summary>
+    /// Represents a message returned in a response from the Cloudflare API.
+    /// </summary>
+    public readonly struct ResponseMessage
+    {
+        [JsonPropertyName("code")]
+        public int Code { get; init; }
+
+        [JsonPropertyName("message")]
+        public string Message { get; init; }
+
+        public override string ToString()
+        {
+            return $"ResponseMessage: " + $"Code: {Code}, " + $"Message: {Message}";
+        }
+    }
+}
