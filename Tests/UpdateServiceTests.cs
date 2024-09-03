d7feb07cd6910a5e7ed9c3383c899b64738b4333
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-09-03 12:56:09 -0700

    additional tests

--- Tests/UpdateServiceTests.cs
+++ Tests/UpdateServiceTests.cs
@@ -112,12 +112,162 @@
             Assert.Equal("321.321.321.321", record.Address);
         }
 
+        [Fact]
+        public void RecordExists_RecordExists_ReturnsTrue()
+        {
+            List<RecordResponse> records =
+            [
+                new RecordResponse()
+                {
+                    Id = "023e105f4ecef8ad9ca31a8372d0c353",
+                    ZoneId = "023e105f4ecef8ad9ca31a8372d0c353",
+                    ZoneName = "example.com",
+                    Name = "example.com",
+                    Type = "A",
+                    Content = "123.123.123.123",
+                    Proxiable = true,
+                    Proxied = true,
+                    TTL = 1,
+                    Meta = new RecordMeta(),
+                    CreatedOn = DateTimeOffset.Now,
+                    ModifiedOn = DateTimeOffset.Now
+                }
+            ];
+            DnsRecord record =
+                new()
+                {
+                    Name = "example.com",
+                    Address = "123.123.123.123",
+                    Proxied = true
+                };
+
+            bool result = UpdateService.RecordExists(records, record);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void RecordExists_RecordDoesNotExist_ReturnsFalse()
+        {
+            List<RecordResponse> records =
+            [
+                new RecordResponse()
+                {
+                    Id = "023e105f4ecef8ad9ca31a8372d0c353",
+                    ZoneId = "023e105f4ecef8ad9ca31a8372d0c353",
+                    ZoneName = "example.com",
+                    Name = "example.com",
+                    Type = "A",
+                    Content = "123.123.123.123",
+                    Proxiable = true,
+                    Proxied = true,
+                    TTL = 1,
+                    Meta = new RecordMeta(),
+                    CreatedOn = DateTimeOffset.Now,
+                    ModifiedOn = DateTimeOffset.Now
+                }
+            ];
+            DnsRecord record =
+                new()
+                {
+                    Name = "wrongname",
+                    Address = "123.123.123.123",
+                    Proxied = true
+                };
+
+            bool result = UpdateService.RecordExists(records, record);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TryCreateNewCloudflareRecord_CurrentAddressIsNull_ThrowsInvalidOperationException()
+        {
+            UpdateService service = GenerateServiceInstance(
+                FakeRecordResponseGenerator.GenerateManyFakeARecords(),
+                GenerateConfig("ConfigWithMultipleA.yaml")
+            );
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                service.TryCreateNewCloudflareRecord(
+                    new DnsRecord()
+                    {
+                        Address = null,
+                        Name = "example.com",
+                        Proxied = true
+                    },
+                    "12345",
+                    CancellationToken.None,
+                    out _
+                );
+            });
+        }
+
+        [Fact]
+        public void TryCreateNewCloudflareRecord_RecordCreated_ReturnsTrue()
+        {
+            UpdateService service = GenerateServiceInstance(
+                FakeRecordResponseGenerator.GenerateManyFakeARecords(),
+                GenerateConfig("ConfigWithMultipleA.yaml")
+            );
+            service.CurrentAddress = new IpifyAddress() { Address = "123.123.123.123" };
+
+            bool result = service.TryCreateNewCloudflareRecord(
+                new DnsRecord()
+                {
+                    Address = null,
+                    Name = "example.com",
+                    Proxied = true
+                },
+                "12345",
+                CancellationToken.None,
+                out _
+            );
+        }
+
+        [Fact]
+        public void TryCreateNewCloudflareRecord_RecordNotCreated_ReturnsFalse()
+        {
+            UpdateService service = GenerateServiceInstance(
+                FakeRecordResponseGenerator.GenerateManyFakeARecords(),
+                GenerateConfig("ConfigWithMultipleA.yaml"),
+                false
+            );
+            service.CurrentAddress = new IpifyAddress() { Address = "123.123.123.123" };
+
+            bool result = service.TryCreateNewCloudflareRecord(
+                new DnsRecord()
+                {
+                    Address = null,
+                    Name = "example.com",
+                    Proxied = true
+                },
+                "12345",
+                CancellationToken.None,
+                out _
+            );
+
+            Assert.False(result);
+        }
+
         public static UpdateService GenerateServiceInstance(
             ListRecordsResponse aRecords,
-            CloudflareConfiguration config
+            CloudflareConfiguration config,
+            bool goodResults = true
         )
         {
-            MockCloudflareClient cfClient = new(aRecords);
+            ICloudflareClient cfClient;
+
+            if (goodResults)
+            {
+                cfClient = new MockCloudflareClientGoodResults(aRecords);
+            }
+            else
+            {
+                cfClient = new MockCloudflareClientBadResults(aRecords);
+            }
+
             MockAddrFetcher addrFetcher = new();
             FakeLogger<UpdateService> logger = new();
 
