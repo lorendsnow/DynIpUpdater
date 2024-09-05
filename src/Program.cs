95d59d90b04bad7eabd5432d46776612f8c3cc95
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-09-04 19:28:31 -0700

    update based on config changes and add required services

--- src/Program.cs
+++ src/Program.cs
@@ -1,27 +1,28 @@
 ﻿namespace DynIpUpdater
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
-            string configPath = args.Length > 0 ? args[0] : string.Empty;
-            if (args.Length == 0)
-            {
-                Console.WriteLine("Please provide a path to a configuration file.");
-                return;
-            }
+            Console.WriteLine("Starting DynIpUpdater");
 
-            Uri baseIpifyAddress = new("https://api.ipify.org");
-            Uri baseCloudflareAddress = new("https://api.cloudflare.com/client/v4");
+            Uri baseIpifyAddress = new("https://api.ipify.org/");
+            Uri baseCloudflareAddress = new("https://api.cloudflare.com/client/v4/");
 
             HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
-            builder.ParseConfigFile(configPath);
+            Console.WriteLine("Loading configuration file...");
+            CloudflareConfiguration config = LoadConfiguration.ParseConfigFile(args);
 
             builder.Services.AddHostedService<UpdateService>();
             builder.Services.AddSingleton(baseCloudflareAddress);
             builder.Services.AddIpifyClient(baseIpifyAddress);
             builder.Services.AddCloudflareClient();
+            builder.Services.AddSingleton<IAddrFetcher, IpifyFetcher>();
+            builder.Services.AddSingleton<ICloudflareClient, CloudflareClient>();
+            builder.Services.AddSingleton(config);
+
+            builder.ConfigureLogging(config.Verbosity);
 
             IHost host = builder.Build();
 
