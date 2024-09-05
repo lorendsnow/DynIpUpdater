698edfcfc24752014ef7075d5cae45c2da7f2309
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-09-04 19:28:05 -0700

    add explainer on Generate method

--- Tests/Helpers/PathGenerator.cs
+++ Tests/Helpers/PathGenerator.cs
@@ -2,6 +2,15 @@
 {
     public static partial class PathGenerator
     {
+        /// <summary>
+        /// When running tests, the working directory is /..../Tests/bin/Debug/net8.0. Because we
+        /// don't include bin files in the repo, we need to dynamically generate the path back to
+        /// the Test folder root to access the test config files.
+        /// </summary>
+        /// <param name="fileName">
+        /// The name of a config file located in the Tests folder that we want to access.
+        /// </param>
+        /// <returns>A fully qualified path to the requested config file.</returns>
         public static string Generate(string fileName) =>
             ConfigPath().Replace(Path.GetFullPath("./"), fileName);
 
