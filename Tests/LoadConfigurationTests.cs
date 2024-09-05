b75fa237db2ca1cf5c3e8d549614e7b8f22609bd
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-09-04 19:24:44 -0700

    rename to loadconfiguration; add additional config steps

---
+++ Tests/LoadConfigurationTests.cs
@@ -0,0 +1,93 @@
+﻿using Xunit.Abstractions;
+using YamlDotNet.Core;
+
+namespace Tests
+{
+    public class LoadConfigurationTests(ITestOutputHelper output)
+    {
+        private readonly ITestOutputHelper _output = output;
+
+        [Fact]
+        public void TryLoadFile_WithValidFullPath_ReturnsFileContents()
+        {
+            string path = PathGenerator.Generate("ConfigWithMultipleA.yaml");
+
+            string fileContents = LoadConfiguration.TryLoadFile(path);
+
+            Assert.Equal("Interval: 1", fileContents[..11]);
+        }
+
+        [Fact]
+        public void TryLoadFile_WithValidRelativePath_ReturnsFileContents()
+        {
+            // See PathGenerator.cs for how this works and why we're doing this.
+            File.Copy(
+                PathGenerator.Generate("ConfigWithMultipleA.yaml"),
+                "ConfigWithMultipleA.yaml"
+            );
+
+            string fileContents = LoadConfiguration.TryLoadFile("ConfigWithMultipleA.yaml");
+
+            Assert.Equal("Interval: 1", fileContents[..11]);
+
+            // Clean up after ourselves
+            File.Delete("ConfigWithMultipleA.yaml");
+        }
+
+        [Fact]
+        public void TryLoadFile_WithInvalidPath_ThrowsFileNotFoundException()
+        {
+            Assert.Throws<FileNotFoundException>(
+                () => LoadConfiguration.TryLoadFile("InvalidPath.yaml")
+            );
+        }
+
+        [Fact]
+        public void DeserializeConfig_WithValidConfig_ReturnsCloudflareConfiguration()
+        {
+            string path = PathGenerator.Generate("ConfigWithMultipleA.yaml");
+
+            string fileContents = LoadConfiguration.TryLoadFile(path);
+
+            CloudflareConfiguration config = LoadConfiguration.DeserializeConfig(fileContents);
+
+            Assert.Equal(1, config.Interval);
+        }
+
+        [Fact]
+        public void DeserializeConfig_WithInvalidConfig_ThrowsException()
+        {
+            string path = PathGenerator.Generate("BadConfig.yaml");
+
+            string fileContents = LoadConfiguration.TryLoadFile(path);
+
+            Assert.Throws<YamlException>(() => LoadConfiguration.DeserializeConfig(fileContents));
+        }
+
+        [Fact]
+        public void DeserializeConfig_WithMissingFields_ReturnsDefaultValues()
+        {
+            string path = PathGenerator.Generate("MissingFields.yaml");
+
+            string fileContents = LoadConfiguration.TryLoadFile(path);
+
+            CloudflareConfiguration config = LoadConfiguration.DeserializeConfig(fileContents);
+
+            Assert.Equal(5, config.Interval);
+            Assert.Equal(3, config.Verbosity);
+            Assert.Equal(3, config.Zones[0].DnsRecords.Count);
+            Assert.Single(config.Zones[1].DnsRecords);
+            foreach (ZoneConfiguration zone in config.Zones)
+            {
+                foreach (DnsRecord record in config.Zones[0].DnsRecords)
+                {
+                    Assert.Null(record.Address);
+                    Assert.Null(record.Comment);
+                    Assert.Null(record.Id);
+                    Assert.Empty(record.Tags);
+                    Assert.Equal(1, record.TTL);
+                }
+            }
+        }
+    }
+}
---
+++ src/LoadConfiguration.cs
@@ -0,0 +1,107 @@
+﻿using YamlDotNet.Core;
+
+namespace DynIpUpdater
+{
+    public static class LoadConfiguration
+    {
+        /// <summary>
+        /// Loads a YAML configuration file and creates a strongly typed configuration object.
+        /// </summary>
+        /// <param name="args">Command line args (if any).</param>
+        /// <returns>A strongly typed configuration object.</returns>
+        public static CloudflareConfiguration ParseConfigFile(string[] args)
+        {
+            string path = args.Length > 0 ? args[0] : "config.yaml";
+
+            string rawConfig = TryLoadFile(path);
+
+            return DeserializeConfig(rawConfig);
+        }
+
+        /// <summary>
+        /// Tries to load a configuration file from the given path.
+        /// </summary>
+        /// <param name="path">The path to the file to load.</param>
+        /// <returns>The contents of the file.</returns>
+        /// <exception cref="FileNotFoundException">
+        /// Thrown if the file does not exist or is not accessible.
+        /// </exception>
+        public static string TryLoadFile(string path)
+        {
+            string fullPath = Path.IsPathFullyQualified(path) ? path : Path.GetFullPath(path);
+
+            if (!File.Exists(fullPath))
+            {
+                throw new FileNotFoundException(
+                    $"Couldn't find config file: {fullPath}, or it was not accessible."
+                );
+            }
+
+            using StreamReader reader = new(fullPath);
+            return reader.ReadToEnd();
+        }
+
+        /// <summary>
+        /// Deserializes a raw YAML configuration string into a strongly typed
+        /// CloudflareConfiguration object.
+        /// </summary>
+        /// <param name="rawConfig">The raw YAML configuration string.</param>
+        /// <returns>A strongly typed CloudflareConfiguration object.</returns>
+        public static CloudflareConfiguration DeserializeConfig(string rawConfig)
+        {
+            /*
+             * TODO: write a custom deserializer/parser so that we can throw better error
+             * descriptions of what is wrong with the config when we have invalid configs.
+             */
+            var deserializer = new DeserializerBuilder().Build();
+
+            return deserializer.Deserialize<CloudflareConfiguration>(rawConfig);
+        }
+
+        /// <summary>
+        /// Configures the logging level for the application.
+        /// </summary>
+        /// <param name="builder">The <see cref="HostApplicationBuilder"/> to configure.</param>
+        /// <param name="verbosity">The verbosity level to set.</param>
+        /// <returns>The <see cref="HostApplicationBuilder"/>.</returns>
+        public static HostApplicationBuilder ConfigureLogging(
+            this HostApplicationBuilder builder,
+            int verbosity
+        )
+        {
+            switch (verbosity)
+            {
+                case 0:
+                    builder.Logging.SetMinimumLevel(LogLevel.None);
+                    break;
+                case 1:
+                    builder.Logging.SetMinimumLevel(LogLevel.Error);
+                    break;
+                case 2:
+                    builder.Logging.SetMinimumLevel(LogLevel.Warning);
+                    break;
+                case 3:
+                    builder.Logging.SetMinimumLevel(LogLevel.Information);
+                    builder.Logging.AddFilter("Microsoft", LogLevel.Warning);
+                    builder.Logging.AddFilter("System", LogLevel.Warning);
+                    break;
+                case 4:
+                    builder.Logging.SetMinimumLevel(LogLevel.Information);
+                    builder.Logging.AddFilter("Microsoft", LogLevel.Information);
+                    builder.Logging.AddFilter("System", LogLevel.Information);
+                    break;
+                default:
+                    Console.WriteLine(
+                        $"Invalid verbosity level {verbosity}, defaulting to 3. Verbosity must be "
+                            + "an integer value of 0, 1, 2, 3 or 4."
+                    );
+                    builder.Logging.SetMinimumLevel(LogLevel.Information);
+                    builder.Logging.AddFilter("Microsoft", LogLevel.Warning);
+                    builder.Logging.AddFilter("System", LogLevel.Warning);
+                    break;
+            }
+
+            return builder;
+        }
+    }
+}
